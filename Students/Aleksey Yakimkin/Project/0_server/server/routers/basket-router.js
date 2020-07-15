0d8941a555550aa0f6e96f7000d21efec982fda8
lehanich <ayakimkin@gmail.com>
2020-07-15 22:56:03 +0300

    повтор урока 7

--- Students/Aleksey Yakimkin/Project/0_server/server/routers/basket-router.js
+++ Students/Aleksey Yakimkin/Project/0_server/server/routers/basket-router.js
@@ -1,69 +1,77 @@
 let express = require('express');
-let logger = require('../Utils/logger');
-let writer = require('../Utils/writer');
-let fs = require('fs');
-let basketServices = require('../Services/basket');
+let { load, add, change } = require('../controllers/basket_controller.js');
+
+// let logger = require('../Utils/logger');
+// let writer = require('../Utils/writer');
+// let fs = require('fs');
+// let basketServices = require('../Services/basket');
 
 let router = express.Router();
 
-router.get('/', (req, res) => {
-    fs.readFile('./server/db/basket.json', 'utf-8', (err, data) => {
-        if (!err) {
-            res.send(data);
-        }
-    })
-});
+router.get('/:id', load);
+router.post('/:id', add);
+router.put('/:id', change);
+// router.get('/:id', change);
+// router.delete('/:id', change);
 
-router.post('/', (req, res) => {
-    fs.readFile('./server/db/basket.json', 'utf-8', (err, data) => {
-        if (!err) {
-            let { newBasket} = basketServices.add(JSON.parse(data), req.body);
-            writer('./server/db/basket.json', newBasket)
-                .then(status => {
-                    if (status) {
-                        res.json({ status: 1 });
-                        logger('add', req.body.product_name);
+// router.get('/', (req, res) => {
+//     fs.readFile('./server/db/basket.json', 'utf-8', (err, data) => {
+//         if (!err) {
+//             res.send(data);
+//         }
+//     })
+// });
 
-                    } else {
-                        res.sendStatus(500);
-                    }
-                })
-        }
-    });
-});
+// router.post('/', (req, res) => {
+//     fs.readFile('./server/db/basket.json', 'utf-8', (err, data) => {
+//         if (!err) {
+//             let { newBasket} = basketServices.add(JSON.parse(data), req.body);
+//             writer('./server/db/basket.json', newBasket)
+//                 .then(status => {
+//                     if (status) {
+//                         res.json({ status: 1 });
+//                         logger('add', req.body.product_name);
 
-router.put('/:id', (req, res) => {
-    fs.readFile('./server/db/basket.json', 'utf-8', (err, data) => {
-        if (!err) {
-            let { newBasket, name } = basketServices.change(JSON.parse(data), req.params.id, req.body.amount);
-            writer('./server/db/basket.json', newBasket)
-                .then(status => {
-                    if (status) {
-                        res.json({ status: 1 });
-                        logger('change', name);
-                    } else {
-                        res.sendStatus(500);
-                    }
-                })
-        }
-    });
-});
+//                     } else {
+//                         res.sendStatus(500);
+//                     }
+//                 })
+//         }
+//     });
+// });
 
-router.delete('/:id', (req, res) => {
-    fs.readFile('./server/db/basket.json', 'utf-8', (err, data) => {
-        if (!err) {
-            let { newBasket, name } = basketServices.delete(JSON.parse(data), req.params.id);
-            writer('./server/db/basket.json', newBasket)
-                .then(status => {
-                    if (status) {
-                        res.json({ status: 1 });
-                        logger('remove', name);
-                    } else {
-                        res.sendStatus(500);
-                    }
-                })
-        }
-    });
-});
+// router.put('/:id', (req, res) => {
+//     fs.readFile('./server/db/basket.json', 'utf-8', (err, data) => {
+//         if (!err) {
+//             let { newBasket, name } = basketServices.change(JSON.parse(data), req.params.id, req.body.amount);
+//             writer('./server/db/basket.json', newBasket)
+//                 .then(status => {
+//                     if (status) {
+//                         res.json({ status: 1 });
+//                         logger('change', name);
+//                     } else {
+//                         res.sendStatus(500);
+//                     }
+//                 })
+//         }
+//     });
+// });
+
+// router.delete('/:id', (req, res) => {
+//     fs.readFile('./server/db/basket.json', 'utf-8', (err, data) => {
+//         if (!err) {
+//             let { newBasket, name } = basketServices.delete(JSON.parse(data), req.params.id);
+//             writer('./server/db/basket.json', newBasket)
+//                 .then(status => {
+//                     if (status) {
+//                         res.json({ status: 1 });
+//                         logger('remove', name);
+//                     } else {
+//                         res.sendStatus(500);
+//                     }
+//                 })
+//         }
+//     });
+// });
 
 module.exports = router;
